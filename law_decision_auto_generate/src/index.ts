e62792db0766e7e64462f7fc6af7ed2d24793e8a
Your Name <you@example.com>
2022-12-10 03:38:45 +0800

    1

--- law_decision_auto_generate/src/index.ts
+++ law_decision_auto_generate/src/index.ts
@@ -9,12 +9,12 @@ const game_base_dir = 'D:\\SteamLibrary\\steamapps\\common\\Victoria 3\\game\\co
 
 // handle file 
 const handleLawFile = (fileName: string, content: string) => {
-    const groupName_name = policy_group.flatMap(item => item.subs.map(i2 => ({ gn: item.name, n: i2, gn_n: item.name + '_' + i2}))).find( item => fileName.includes(item.n))?.gn_n || '';
+    const policy_name = policy_group.flatMap( item => item.subs).find( sub => fileName.includes(sub))|| fileName;
     const parsedContent = parse_file(content);
     const options = parsedContent.map( (item, index) => (
     `
     option = {
-        name = purge_your_vassal_policy.${groupName_name }.events.1.choose.${index}
+        name = purge_your_vassal_policy_${policy_name}.events.1.choose.${index}
         scope:target_country = {
             activate_law = law_type:${item.getName()}
         }
@@ -23,17 +23,17 @@ const handleLawFile = (fileName: string, content: string) => {
     ).join('\n');
     // generate the mod file 
     const template = fs.readFileSync( path.join(__dirname,'../template/policy_temp.txt')).toString();
-    const res = template.replace(/###policy_name###/g, groupName_name).replace(/###options###/g, options);
+    const res = template.replace(/###policy_name###/g, policy_name).replace(/###options###/g, options);
     return res;
 }
 const generateMenu = async () => {
     const template = fs.readFileSync( path.join(__dirname,'../template/menu_temp.txt')).toString();
     const menus = policy_group.map( policy => {
-        const options = policy.subs.map( (item) => (
+        const options = policy.subs.map( (item, index) => (
     `
     option = {
-        name = purge_your_vassal_policy_${policy.name}_events.choose.${item}
-        trigger_event = { id = purge_your_vassal_policy_${policy.name}_${item}.1 popup = yes }
+        name = purge_your_vassal_policy_${policy.name}_events.choose.${index}
+        trigger_event = { id = purge_your_vassal_policy_${item}.1 popup = yes }
     }
     `
     )).join('\n');
