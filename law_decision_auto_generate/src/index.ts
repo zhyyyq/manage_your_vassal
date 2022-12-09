3c9e4e8f8893d27f3419b3504f014f6337384e44
Your Name <you@example.com>
2022-12-10 02:08:07 +0800

    feat: save

--- law_decision_auto_generate/src/index.ts
+++ law_decision_auto_generate/src/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { parse_file } from './utils';
 const game_base_dir = 'D:\\SteamLibrary\\steamapps\\common\\Victoria 3\\game\\common\\laws';
-const dir = fs.readdirSync(game_base_dir);
+
 
 
 
@@ -22,16 +22,29 @@ const handleLawFile = (fileName: string, content: string) => {
     // generate the mod file 
     const template = fs.readFileSync( path.join(__dirname,'../template/policy_temp.txt')).toString();
     const res = template.replace(/###policy_name###/g, fileName).replace(/###options###/, options);
-    fs.writeFileSync(`${path.join(__dirname,`../dist/purge_your_vassal_policy_${fileName}`)}`, res );
+    fs.writeFileSync(`${path.join(__dirname,`../dist/options/purge_your_vassal_policy_${fileName}`)}`, res );
+}
+const generateMenu = async (fileNames: string[]) => {
+    const template = fs.readFileSync( path.join(__dirname,'../template/menu_temp.txt')).toString();
+    const options = fileNames.map( (item, index) => (
+        `
+        option = {
+            name = purge_your_vassal_policy_1_events.1.choose.${index}
+            trigger_event = { id = purge_your_vassal_policy_${item}.1 popup = yes }
+        }
+        `
+    )).join('\n');
+    const res = template.replace('###options###', options);
+    fs.writeFileSync(`${path.join(__dirname,`../dist/purge_your_vassal_policy_menu.txt`)}`, res );
 }
 
-const task = dir.map( async file => {
-    const fileContent = fs.readFileSync(game_base_dir + '/' + file );
-    handleLawFile(file, fileContent.toString());
-});
-
 const main = async () => {
-    await Promise.all(task);
+    const dir = fs.readdirSync(game_base_dir);
+    const task = dir.map( async file => {
+        const fileContent = fs.readFileSync(game_base_dir + '/' + file );
+        handleLawFile(file, fileContent.toString());
+    });
+    await Promise.all([...task,generateMenu(dir)]);
 }
 
 main();
